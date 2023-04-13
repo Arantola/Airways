02cef46dc64e189aa8ca70575e06e6404630710c
Arseny Fedorchenko <Arantola@mail.ru>
2023-04-13 11:36:43 +0300

    feat: add core module and content-container component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../shared/material/material.module';
+import { ContentContainerComponent } from './pages/content-container/content-container.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+const CoreComponents = [ContentContainerComponent];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, MaterialModule],
+  declarations: [CoreComponents],
+  exports: [CoreComponents],
+  imports: [CommonModule, AppRoutingModule, MaterialModule],
 })
 export class CoreModule {}
