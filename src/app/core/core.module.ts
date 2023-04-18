eee0498ac41ba8a471aea329aff99e736db2dd8a
ULADZISLAVA <vladislava96.96@mail.ru>
2023-04-18 11:25:18 +0300

    feat: add stepper

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,12 +5,13 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ContentContainerComponent } from './pages/content-container/content-container.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { SecondMenuComponent } from './components/second-menu/second-menu.component';
+import { StepperComponent } from './components/stepper/stepper.component';
 
 const CoreComponents = [ContentContainerComponent, SecondMenuComponent, FooterComponent];
 
 @NgModule({
-  declarations: [CoreComponents],
-  exports: [CoreComponents],
+  declarations: [CoreComponents, StepperComponent],
+  exports: [CoreComponents, StepperComponent],
   imports: [CommonModule, AppRoutingModule, MaterialModule],
 })
 export class CoreModule {}
