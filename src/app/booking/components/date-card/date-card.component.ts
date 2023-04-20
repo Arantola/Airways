c7ea6a77ca245c5afe7ca862e0517a3447207c8c
ULADZISLAVA <vladislava96.96@mail.ru>
2023-04-20 21:25:20 +0300

    feat: add styles for different state

--- src/app/booking/components/date-card/date-card.component.ts
+++ src/app/booking/components/date-card/date-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-date-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./date-card.component.scss']
 })
 export class DateCardComponent {
+  @Input() public isSelect = false;
+  @Input() public isDisable = true;
 
 }
