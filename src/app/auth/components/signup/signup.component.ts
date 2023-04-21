70bb6394972701cd819f788d77f50948c4b65b57
Nikita Ilyanov <nikita.glk@mail.ru>
2023-04-21 03:02:50 +0300

    feat: add validation to sign up form

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import nationalities from 'src/app/shared/constants/nationalities.json';
+import phoneCode from 'src/app/shared/constants/CountryCodes.json';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  signUpForm!: FormGroup;
+  citizenshipsData = nationalities;
+  countryCodeData = phoneCode;
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      'userEmail': ['', [Validators.required, Validators.email]],
+      'userPassword': ['', Validators.required],
+      'userFirstName': ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+      'userLastName': ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+      'userBirthday': ['', [Validators.required, this.dateValidator]],
+      'userGender': [''],
+      'countryCode': [''],
+      'phoneNumber': ['', [Validators.required, Validators.pattern(/^\+?(\d{1,3})?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/)]],
+      'nationalities': [''],
+    })
+  }
+
+  dateValidator(control: FormControl): { [s: string]: boolean } | null {
+    const creationDate = new Date(control.value);
+    const nowDate = new Date();
+    if (creationDate > nowDate) {
+      return { futureDate: true };
+    }
+    return null;
+  }
 
 }
