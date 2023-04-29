1324c4ce67968a112f577f3abea99fe7518ff827
Nikita Ilyanov <nikita.glk@mail.ru>
2023-04-30 01:06:49 +0300

    feat: add styles to summary page

--- src/app/booking/components/flight-info/flight-direction/flight-direction.component.ts
+++ src/app/booking/components/flight-info/flight-direction/flight-direction.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./flight-direction.component.scss']
 })
 export class FlightDirectionComponent {
+  flightInfo = {
+    numberFlight: 'FR 1925',
+    departure: 'Dublin',
+    arrival: 'Warsaw Modlin',
+    date: 'Wednesday, 1 March, 2023',
+    departureTime: '8:40',
+    arrivalTime: '12:00',
+  }
 
 }
