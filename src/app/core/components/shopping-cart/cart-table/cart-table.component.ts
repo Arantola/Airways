c9285ed4ced0e99932a982110312c40fba0bef67
ULADZISLAVA <vladislava96.96@mail.ru>
2023-05-21 23:30:25 +0300

    feat: start implement shopping cart

--- src/app/core/components/shopping-cart/cart-table/cart-table.component.ts
+++ src/app/core/components/shopping-cart/cart-table/cart-table.component.ts
@@ -1,35 +1,21 @@
-import { AfterViewInit, Component, ViewChild, Input } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Router } from '@angular/router';
 import { BOOKING_PAGES, CART_COLUMNS } from 'src/app/shared/constants/constants';
 import { Store } from '@ngrx/store';
-import { selectCurrentOrder } from 'src/app/redux/selectors/app.selectors';
-import { combineLatest, tap } from 'rxjs';
-import { selectPassengersCompound } from 'src/app/redux/selectors/app.selectors';
-import { PassengersCompound } from 'src/app/shared/interfaces/interfaces';
-import { DatePipe } from '@angular/common';
-import { formatDate } from '@angular/common';
-
-interface PeriodicElement {
-  no: string | undefined;
-  flight: string;
-  typeTrip: string;
-  dataTime: string[];
-  passengers: PassengersCompound;
-  price: number | undefined;
-}
+import { PeriodicElement } from '../cart/cart.component';
 
 @Component({
   selector: 'app-cart-table',
   templateUrl: './cart-table.component.html',
   styleUrls: ['./cart-table.component.scss']
 })
-export class CartTableComponent implements AfterViewInit{
-  ELEMENT_DATA: PeriodicElement[] = [];
+export class CartTableComponent implements AfterViewInit, OnChanges {
+  @Input() tableData: PeriodicElement[] = [];
 
-  dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  @Input() allOrdersSelected = false;
 
   @Input() showControls = true;
 
@@ -37,24 +23,11 @@ export class CartTableComponent implements AfterViewInit{
 
   @Input() currency = 'EUR';
 
-  private order$ = this.store.select(selectCurrentOrder).subscribe(
-    (order) => {
-      let obj: PeriodicElement = {
-        no: order.selectedFlightFrom?.flight?.id,
-        flight: `${order.departurePoint?.city} — ${order.destinationPoint?.city} — ${order.departurePoint?.city}`,
-        typeTrip: order.isRounded ? 'Round trip' : 'One way',
-        dataTime: [
-          `${this.transformDateFormat(order.selectedFlightFrom?.flight?.date)},
-          ${order.selectedFlightFrom?.flight?.startTime} — ${order.selectedFlightFrom?.finishTime}`,
-          `${this.transformDateFormat(order.selectedFlightBack?.flight?.date)},
-          ${order.selectedFlightBack?.flight?.startTime} — ${order.selectedFlightBack?.finishTime}`
-        ],
-        passengers: order.passengersCompound,
-        price: 0,
-      }
-      this.ELEMENT_DATA.push(obj);
-    }
-  );
+  public dataSource = new MatTableDataSource(this.tableData);
+
+  public isOrdersSelected = false;
+
+  @Output() orderSubmit = new EventEmitter<string>();
 
   @ViewChild(MatSort)
   sort?: MatSort;
@@ -65,14 +38,17 @@ export class CartTableComponent implements AfterViewInit{
     private store: Store
   ) { }
 
-  private transformDateFormat(date: string | undefined) {
-    if (date === undefined) {
-      return;
+
+  ngOnChanges(changes: SimpleChanges) {
+    const { tableData, isOrdersSelected } = changes;
+    if (tableData !== undefined) {
+      this.dataSource.data = tableData.currentValue;
     }
-    const newDate = formatDate(date, 'd, MMM, YYYY', 'en')
-    return newDate;
   }
 
+  onOrderSelected(no: string) {
+    this.orderSubmit.emit(no)
+  }
 
   public get displayedColumns(): string[] { 
     let columns = CART_COLUMNS;
--- src/app/core/components/shopping-cart/cart/cart.component.ts
+++ src/app/core/components/shopping-cart/cart/cart.component.ts
@@ -2,7 +2,17 @@ import { Store } from '@ngrx/store';
 import { appSettingsActions } from 'src/app/redux/actions/app.actions';
 import { Component, Input, OnInit } from '@angular/core';
 import { selectCurrentOrder } from 'src/app/redux/selectors/app.selectors';
+import { formatDate } from '@angular/common';
+import { PassengersCompound } from 'src/app/shared/interfaces/interfaces';
 
+export interface PeriodicElement {
+  no: string | undefined;
+  flight: string;
+  typeTrip: string;
+  dataTime: string[];
+  passengers: PassengersCompound;
+  price: number | undefined;
+}
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -13,7 +23,30 @@ export class CartComponent implements OnInit {
 
   public currency = 'EUR';
 
-  private order$ = this.store.select(selectCurrentOrder);
+  public allOrdersSelected = false;
+
+  public tableData: PeriodicElement[] = [];
+
+  private ordersPayable = [];
+
+  private order$ = this.store.select(selectCurrentOrder).subscribe(
+    (order) => {
+      let obj: PeriodicElement = {
+        no: order.selectedFlightFrom?.flight?.id,
+        flight: `${order.departurePoint?.city} — ${order.destinationPoint?.city} — ${order.departurePoint?.city}`,
+        typeTrip: order.isRounded ? 'Round trip' : 'One way',
+        dataTime: [
+          `${this.transformDateFormat(order.selectedFlightFrom?.flight?.date)},
+          ${order.selectedFlightFrom?.flight?.startTime} — ${order.selectedFlightFrom?.finishTime}`,
+          `${this.transformDateFormat(order.selectedFlightBack?.flight?.date)},
+          ${order.selectedFlightBack?.flight?.startTime} — ${order.selectedFlightBack?.finishTime}`
+        ],
+        passengers: order.passengersCompound,
+        price: 0,
+      }
+      this.tableData.push(obj);
+    }
+  );
 
   constructor(private store: Store) {}
 
@@ -21,19 +54,40 @@ export class CartComponent implements OnInit {
     this.store.dispatch(appSettingsActions.changePage({ currentPage: 'cart' }));
     const prices: number[] = []
 
-    const orders = [];
+  //   const orders = [];
     
-    // const prices: number[] = []
+  //   const prices: number[] = []
 
-    // this.tickets$.subscribe((tickets) => {
-    //   for (let ticket of tickets) {
-    //     prices.push(ticket.price ?? 0);
-    //   }
-    // })
-    // this.sum = prices.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  //   this.tickets$.subscribe((tickets) => {
+  //     for (let ticket of tickets) {
+  //       prices.push(ticket.price ?? 0);
+  //     }
+  //   })
+  //   this.sum = prices.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
 
-    // this.settings$.subscribe((settings) => {
-    //   this.currency = settings.currency
-    // })
+  //   this.settings$.subscribe((settings) => {
+  //     this.currency = settings.currency
+  //   })
+  }
+
+  private transformDateFormat(date: string | undefined) {
+    if (date === undefined) {
+      return;
+    }
+    const newDate = formatDate(date, 'd, MMM, YYYY', 'en')
+    return newDate;
+  }
+
+  public onAllOrdersSelected() {
+    if (this.allOrdersSelected) {
+      //выбираем все заказы и пушим в ordersPayable
+    } else {
+      //удаляем все заказы
+    }
+  }
+
+  public addOrderToPayment(no: string) {
+    //берем все заказы и ищем по номеру, после добавляем в массив
+    //если ordersPayable === orders, то allOrdersSelected === true
   }
 }
