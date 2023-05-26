b4e0dd20c7e678181f864b1baa9e940e4f2c9d75
ULADZISLAVA <vladislava96.96@mail.ru>
2023-05-26 12:14:51 +0300

    feat: implement a reusable table

--- src/app/core/components/shopping-cart/cart-table/cart-table.component.ts
+++ src/app/core/components/shopping-cart/cart-table/cart-table.component.ts
@@ -5,63 +5,81 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Router } from '@angular/router';
 import { BOOKING_PAGES, CART_COLUMNS } from 'src/app/shared/constants/constants';
 import { Store } from '@ngrx/store';
-import { PeriodicElement } from '../cart/cart.component';
 import { MatCheckboxChange } from '@angular/material/checkbox';
+import { formatDate } from '@angular/common';
+import { bookingActions, ordersActions } from 'src/app/redux/actions/app.actions';
+import { CurrentOrder, PassengersCompound, UserOrder } from 'src/app/shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-cart-table',
   templateUrl: './cart-table.component.html',
   styleUrls: ['./cart-table.component.scss']
 })
-export class CartTableComponent implements AfterViewInit, OnChanges {
-  @Input() tableData: PeriodicElement[] = [];
-
-  @Input() allOrdersSelected = false;
+export class CartTableComponent implements AfterViewInit {
 
   @Input() showControls = true;
-
   @Input() showLink = false;
-
   @Input() currency = 'EUR';
+  @Input() orders?: UserOrder[];
+  @Input() ordersPayable: UserOrder[] = [];
 
-  public dataSource = new MatTableDataSource(this.tableData);
+  @Output() changeOrdersPayable = new EventEmitter<UserOrder[]>();
 
-  @Output() addingSelectedOrder = new EventEmitter<string>();
+  public allOrdersSelected = false;
 
-  @Output() orderDeleted = new EventEmitter<string>();
-
-  @Output() orderEdited = new EventEmitter<string>();
+  public dataSource = new MatTableDataSource<UserOrder>();
 
   @ViewChild(MatSort)
   sort?: MatSort;
 
   constructor(
-    private _liveAnnouncer: LiveAnnouncer,
+    private store: Store,
     private router: Router,
-    private store: Store
-  ) { }
-
+    private _liveAnnouncer: LiveAnnouncer
+  ) {
+  }
 
   ngOnChanges(changes: SimpleChanges) {
-    const { tableData, isOrdersSelected } = changes;
-    if (tableData !== undefined) {
-      this.dataSource.data = tableData.currentValue;
+    const { orders } = changes;
+    if (orders !== undefined) {
+      this.dataSource.data = orders.currentValue;
     }
   }
 
-  public orderSelected(event: MatCheckboxChange) {
-    console.log(event.source.value)
-    console.log(event.checked)
-    if (event.checked === true)
-    this.addingSelectedOrder.emit(event.source.value)
+  private transformDateFormat(date: string | undefined) {
+    if (date === undefined) {
+      return;
+    }
+    const newDate = formatDate(date, 'd, MMM, YYYY', 'en')
+    return newDate;
+  }
+
+  public deleteOrder(id: string) {
+    this.store.dispatch(ordersActions.deleteOrder({ id }));
   }
 
   public editOrder(id: string) {
-    this.orderEdited.emit(id)
+    const userOrder = this.orders?.find((userOrder) => Object.keys(userOrder)[0] === id);
+    if (userOrder === undefined) {
+      return;
+    }
+    const currentOrder = this.getElementData(userOrder);
+    this.store.dispatch(bookingActions.updateFirstForm({ currentOrder }))
     this.router.navigate(['/main', BOOKING_PAGES[0]])
   }
-  public deleteOrder(id: string) {
-    this.orderDeleted.emit(id)
+
+  public orderSelected(event: MatCheckboxChange, userOrder: UserOrder) {
+    if (event.checked) {
+      this.ordersPayable.push(userOrder);
+    } else {
+      const index = this.ordersPayable.indexOf(userOrder);
+      this.ordersPayable.splice(index, 1);
+    }
+    this.changeOrdersPayable.emit(this.ordersPayable);
+  }
+
+  isOrderSelected(userOrder: UserOrder): boolean {
+    return this.ordersPayable.indexOf(userOrder) >= 0;
   }
 
   public get displayedColumns(): string[] { 
@@ -83,6 +101,8 @@ export class CartTableComponent implements AfterViewInit, OnChanges {
     }
 
     return columns;
+
+    return columns;
   }
 
   ngAfterViewInit() {
@@ -97,5 +117,78 @@ export class CartTableComponent implements AfterViewInit, OnChanges {
     } else {
       this._liveAnnouncer.announce('Sorting cleared');
     }
+  }
+
+  getElementId(userOrder: UserOrder): string {
+    return Object.keys(userOrder)[0];
+  }
+
+  getElementData(userOrder: UserOrder): CurrentOrder {
+    const id = this.getElementId(userOrder);
+    return userOrder[id];
+  }
+
+  getElementNo(userOrder: UserOrder): string | undefined {
+    const order = this.getElementData(userOrder);
+    return order.selectedFlightFrom?.flight?.id;
+  }
+
+  getElementFlight(userOrder: UserOrder): string | undefined {
+    const order = this.getElementData(userOrder);
+
+    if (
+      order.destinationPoint === undefined ||
+      order.departurePoint === undefined
+    ) {
+      return;
+    }
+
+    let wallBack = '';
+    if (order.isRounded) {
+      wallBack = ` — ${order.departurePoint.city}`;
+    }
+
+    return `${order.departurePoint.city} — ${order.destinationPoint.city}${wallBack}`;
+  }
+
+  getElementTypeTrip(userOrder: UserOrder): string {
+    const order = this.getElementData(userOrder);
+
+    return order.isRounded ? 'Round trip' : 'One way';
+  }
+
+  getElementDates(userOrder: UserOrder): string[] {
+    const order = this.getElementData(userOrder);
+
+    const dates = [
+      `${this.transformDateFormat(order.selectedFlightFrom?.flight?.date)},
+        ${order.selectedFlightFrom?.flight?.startTime} —
+        ${order.selectedFlightFrom?.finishTime}`,
+    ];
+
+    if (order.isRounded) {
+      dates.push(`${this.transformDateFormat(order.selectedFlightBack?.flight?.date)},
+        ${order.selectedFlightBack?.flight?.startTime} —
+        ${order.selectedFlightBack?.finishTime}`);
+    }
+
+    return dates;
+  }
+
+  getElementPassengers(userOrder: UserOrder): PassengersCompound {
+    const order = this.getElementData(userOrder);
+
+    return order.passengersCompound;
+  }
+
+  getElementPrice(userOrder: UserOrder): number | undefined {
+    const order = this.getElementData(userOrder);
+
+    return order.totalCost;
+  }
+
+  goToSummary(userOrder: UserOrder) {
+    const order = this.getElementData(userOrder);
+    this.store.dispatch(bookingActions.updateFirstForm({ currentOrder: order }));
   }
 }
--- src/app/core/components/shopping-cart/cart/cart.component.ts
+++ src/app/core/components/shopping-cart/cart/cart.component.ts
@@ -1,84 +1,45 @@
 import { Store } from '@ngrx/store';
-import { appSettingsActions, bookingActions, ordersActions } from 'src/app/redux/actions/app.actions';
-import { Component, Input, OnInit, ViewChild, AfterViewInit, SimpleChanges } from '@angular/core';
-import { formatDate } from '@angular/common';
-import { CurrentOrder, PassengersCompound, UserOrder } from 'src/app/shared/interfaces/interfaces';
+import { appSettingsActions, ordersActions } from 'src/app/redux/actions/app.actions';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { CurrentOrder, UserOrder } from 'src/app/shared/interfaces/interfaces';
 import { selectOrders } from 'src/app/redux/selectors/orders.selectors';
 import { Subject, takeUntil } from 'rxjs';
-import { Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { BOOKING_PAGES, CART_COLUMNS } from 'src/app/shared/constants/constants';
 import { MatCheckboxChange } from '@angular/material/checkbox';
-import { Router } from '@angular/router';
 import { MatSort } from '@angular/material/sort';
-import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent implements OnInit, AfterViewInit{
-  @Input() public sum?: number;
-
+export class CartComponent implements OnInit {
   public currency = 'EUR';
-
   public allOrdersSelected = false;
-
-  private orders?: UserOrder[];
-
-  private ordersPayable: UserOrder[] = [];
+  public orders?: UserOrder[];
+  public ordersPayable: UserOrder[] = [];
+  public dataSource = new MatTableDataSource<UserOrder>();
 
   private destroy$ = new Subject<void>();
 
-  public dataSource = new MatTableDataSource<UserOrder>();
-
-  @ViewChild(MatSort)
-  sort?: MatSort;
-
   constructor(
     private store: Store,
-    private router: Router,
-    private _liveAnnouncer: LiveAnnouncer
   ) {
     this.store.select(selectOrders).pipe(takeUntil(this.destroy$)).subscribe(
       (orders) => {
         this.orders = orders;
-        this.dataSource.data = orders;
       },
     );
   }
 
   ngOnInit(): void {
-    this.store.dispatch(appSettingsActions.changePage({ currentPage: 'cart' }));
+    this.store.dispatch(appSettingsActions.changePage({ currentPage: 'shopping-cart' }));
     this.store.dispatch(ordersActions.loadOrders());
   }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
-  }
-
-  private transformDateFormat(date: string | undefined) {
-    if (date === undefined) {
-      return;
-    }
-    const newDate = formatDate(date, 'd, MMM, YYYY', 'en')
-    return newDate;
-  }
-
-  public deleteOrder(id: string) {
-    this.store.dispatch(ordersActions.deleteOrder({ id }));
-  }
-
-  public editOrder(id: string) {
-    const userOrder = this.orders?.find((userOrder) => Object.keys(userOrder)[0] === id);
-    if (userOrder === undefined) {
-      return;
-    }
-    const currentOrder = this.getElementData(userOrder);
-    this.store.dispatch(bookingActions.updateFirstForm({ currentOrder }))
-    this.router.navigate(['/main', BOOKING_PAGES[0]])
   }
 
   public onAllOrdersSelected(event: MatCheckboxChange): void {
@@ -93,45 +54,6 @@ export class CartComponent implements OnInit, AfterViewInit{
     return this.orders?.length === this.ordersPayable.length;
   }
 
-  public orderSelected(event: MatCheckboxChange, userOrder: UserOrder) {
-    if (event.checked) {
-      this.ordersPayable.push(userOrder);
-    } else {
-      const index = this.ordersPayable.indexOf(userOrder);
-      this.ordersPayable.splice(index, 1);
-    }
-  }
-
-  public get displayedColumns(): string[] { 
-    let columns = CART_COLUMNS;
-
-    columns = [
-      'checkbox',
-      ...columns,
-      'menu',
-    ]
-
-    return columns;
-  }
-
-  ngAfterViewInit() {
-    if (this.sort !== undefined) {
-      this.dataSource.sort = this.sort;
-    }
-  }
-
-  announceSortChange(sortState: Sort) {
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
-  }
-
-  isOrderSelected(userOrder: UserOrder): boolean {
-    return this.ordersPayable.indexOf(userOrder) >= 0;
-  }
-
   getElementId(userOrder: UserOrder): string {
     return Object.keys(userOrder)[0];
   }
@@ -139,65 +61,6 @@ export class CartComponent implements OnInit, AfterViewInit{
   getElementData(userOrder: UserOrder): CurrentOrder {
     const id = this.getElementId(userOrder);
     return userOrder[id];
-  }
-
-  getElementNo(userOrder: UserOrder): string | undefined {
-    const order = this.getElementData(userOrder);
-    return order.selectedFlightFrom?.flight?.id;
-  }
-
-  getElementFlight(userOrder: UserOrder): string | undefined {
-    const order = this.getElementData(userOrder);
-
-    if (
-      order.destinationPoint === undefined ||
-      order.departurePoint === undefined
-    ) {
-      return;
-    }
-
-    let wallBack = '';
-    if (order.isRounded) {
-      wallBack = ` — ${order.departurePoint.city}`;
-    }
-
-    return `${order.departurePoint.city} — ${order.destinationPoint.city}${wallBack}`;
-  }
-
-  getElementTypeTrip(userOrder: UserOrder): string {
-    const order = this.getElementData(userOrder);
-
-    return order.isRounded ? 'Round trip' : 'One way';
-  }
-
-  getElementDates(userOrder: UserOrder): string[] {
-    const order = this.getElementData(userOrder);
-
-    const dates = [
-      `${this.transformDateFormat(order.selectedFlightFrom?.flight?.date)},
-        ${order.selectedFlightFrom?.flight?.startTime} —
-        ${order.selectedFlightFrom?.finishTime}`,
-    ];
-
-    if (order.isRounded) {
-      dates.push(`${this.transformDateFormat(order.selectedFlightBack?.flight?.date)},
-        ${order.selectedFlightBack?.flight?.startTime} —
-        ${order.selectedFlightBack?.finishTime}`);
-    }
-
-    return dates;
-  }
-
-  getElementPassengers(userOrder: UserOrder): PassengersCompound {
-    const order = this.getElementData(userOrder);
-
-    return order.passengersCompound;
-  }
-
-  getElementPrice(userOrder: UserOrder): number | undefined {
-    const order = this.getElementData(userOrder);
-
-    return order.totalCost;
   }
 
   get totalCost(): number {
